f57f489454fe645ba7d3557d71ecb4e8fb24cc0c
Ramu <37015092+bugudiramu@users.noreply.github.com>
2020-06-24 07:32:18 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import React from 'react';
-import Footer from './Components/Footer/Footer'
-import Contact from './Components/Contact/Contact'
-import About from './Components/About/About'
-import Service from './Components/Service/Service'
-import MobileCard from './Components/FoodCard/MobileCard';
+import React from "react";
+import { connect } from "react-redux";
+import Home from "./containers/Home/Home";
+import SignUp from "./containers/Signup/Signup";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./containers/Login/Login";
+import Nav from "./component/UI/Navigation/Nav/Nav";
+import Service from "./component/Service/SpecialService";
+import Admin from "./containers/Admin/Admin";
+import About from "./component/About/About";
+import Profile from "./containers/Profile/Profile";
+import Search from "./containers/Search/Search";
+import Footer from "./component/Footer/Footer";
+import Error from "./component/Error/Error";
+import VegItems from "./containers/VegItems/VegItems";
+import NonVegItems from "./containers/NonVegItems/VegItems";
+import Cart from "./containers/Cart/Cart";
+import Orders from "./containers/Orders/Orders";
+import Details from "./containers/Details/Details";
+import { toast } from "react-toastify";
 
+const signInRoutes = (
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/admin" component={Admin} />
+    <Route path="/service" component={Service} />
+    <Route path="/veg" component={VegItems} />
+    <Route path="/details/:id" component={Details} />
+    <Route path="/nonveg" component={NonVegItems} />
+    <Route path="/cart" component={Cart} />
+    <Route path="/orders" component={Orders} />
+    <Route path="/about" component={About} />
+    <Route path="/profile" component={Profile} />
+    <Route path="/search" component={Search} />
+    <Route component={Error} />
+  </Switch>
+);
+const signOutRoutes = (
+  <Switch>
+    <Route  path="/" exact component={Login} />
+    <Route path="/signup" exact component={SignUp} />
+    <Route component={Error} />
+  </Switch>
+);
 
-function App() {
+toast.configure()
+function App(props) {
   return (
-    <div>
-      {/* <MobileCard /> */}
-      <Service />
-      <About />
-      <Contact />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Nav />
+      {!localStorage.getItem("signIn") ? signOutRoutes : signInRoutes}
+    </BrowserRouter>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+      isSignIn : state.auth.isSignIn
+  };
+};
+
+export default connect(mapStateToProps)(App);
+
+// NAVIGATION BAR IS FIXES SO WE HAVE TO GIVE MARGIN TOP AND MARGIN BOTTOM TO ENTIRE APP
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import config from "./config/Config";
+const composeEnhancer =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancer(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reactReduxFirebase(config, {useFirestoreForProfile:true,userProfile:'users', attachAuthIsReady: true }),
+    reduxFirestore(config)
+  )
 );
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+  serviceWorker.unregister();
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// * Here  useFirestoreForProfile is for displaying profile data with the same userID,
+// * attachAuthIsReady => for not flashing out the nav links like login logout when refreshing the browser
+// * userProfile => Tell the firebase to which collection to sync for profile
